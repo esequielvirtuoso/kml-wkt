b4d727f90545d8072918501dc22e2bdeff1fe311
Andre Duarte <andre.duarte@neoway.com.br>
2019-08-16 22:25:16 -0300

    bug: corrigindo spelling de tags

--- libs/views.py
+++ libs/views.py
@@ -1,14 +1,18 @@
 def upload_view():
     return '''
         <!doctype html>
+        <html>
         <title>Carregar arquivo KML</title>
         <h1>Carregar arquivos KML</h1>
-        <form method=post enctype=multipart/form-data>
-          <input required type=file name=file> </br></br>
-          <label for='buffer'>Tamanho do buffer:</lable>
-          <input type='text' name='buffer' value='0'> </br></br>
-          <label for='table'>Nome da tabela:</lable>
-          <input required type='text' name='table'> </br></br>
-          <input type=submit value=Upload> </br>
-        </form>
+            <form method=post enctype=multipart/form-data>
+              <input required type=file name=file> </br></br>
+              <label for='buffer'>Tamanho do buffer:</label>
+              <input type='text' name='buffer' value='0'> </br></br>
+              <label for='agregar'>Gerar um geom por arquivo?</label>
+              <input type='checkbox' name='agregar'> </br></br>
+              <label for='table'>Nome da tabela:</label>
+              <input required type='text' name='table'> </br></br>
+              <input type=submit value=Upload> </br>
+            </form>
+        </html>
         '''
