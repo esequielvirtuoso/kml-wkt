b300d699ad0fd206a0e4778466e21dbfa3381e41
Andre Duarte <andre.duarte@neoway.com.br>
2019-08-20 13:39:28 -0300

    refactor: ajustando nome do campo no formulario

--- libs/views.py
+++ libs/views.py
@@ -8,7 +8,7 @@ def upload_view():
               <input required type=file name=file> </br></br>
               <label for='buffer'>Tamanho do buffer:</label>
               <input type='text' name='buffer' value='0'> </br></br>
-              <label for='aggregate'>Gerar um geom por arquivo?</label>
+              <label for='aggregate'>Gerar um geom para cada arquivo kml ou kmz?</label>
               <input type='checkbox' name='aggregate'> </br></br>
               <label for='table'>Nome da tabela:</label>
               <input required type='text' name='table'> </br></br>
