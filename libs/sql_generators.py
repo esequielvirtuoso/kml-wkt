113df0e63e0766f354117f031faefff8dcf22957
Andre Duarte <andre.duarte@neoway.com.br>
2019-08-20 12:14:47 -0300

    feature: script agora insere nome do arquivo raw no lugar do nome do cliente

--- libs/sql_generators.py
+++ libs/sql_generators.py
@@ -1,37 +1,41 @@
 from django.contrib.gis.geos import GEOSGeometry
 import json
+from .utils import slugify
 
+
+# TODO: check out the error: OGC WKT expected, EWKT provided - use GeomFromEWKT() for this that generates on inserts
 
 def create_table_sql(table_name, temp):
     if temp:
         temp_table = 'temp'
     else:
         temp_table = ''
-        table_name = 'sd_producao.' + table_name  # TODO: fix bug
+        table_name = 'sd_producao.' + table_name
 
     return '''create {1} table {0}
         (
             id_geoprocessamento varchar(255) not null,
-            nm_cliente          varchar(255),
+            file_name          varchar(255),
             geom                geometry
         );
 
         create index on {0} (id_geoprocessamento);
-        create index on {0} (nm_cliente);
+        create index on {0} (file_name);
 
         '''.format(table_name, temp_table)
 
 
-def insert_geom_sql(feat_json, filename_slug, buffer):
+def insert_geom_sql(feat_json, buffer, filename):
+    filename_slug = slugify(filename)
     geom = json.dumps(feat_json['geometry'])
     pnt = GEOSGeometry(geom)
 
-    if buffer: #TODO: remover todos os inserts de nome do cliente e adicionar do nome do arquivo
-        comando_sql = "('algar_mpe_{0}', 'ALGAR', ST_Buffer(ST_GeomFromText('{1}',4326)::geography, {2})::GEOMETRY)"
+    if buffer:  # TODO: remover nome do cliente e adicionar do nome do arquivo nos inserts
+        comando_sql = "('algar_mpe_{0}', '{1}', ST_Buffer(ST_GeomFromText('{2}',4326)::geography, {3})::GEOMETRY)"
     else:
-        comando_sql = "('regional_{0}', 'ALGAR', ST_GeomFromText('{1}',4326))"
+        comando_sql = "('regional_{0}', '{1}', ST_GeomFromText('{2}',4326))"
 
-    return comando_sql.format(filename_slug, pnt, buffer)
+    return comando_sql.format(filename_slug, filename, pnt, buffer)
 
 
 def final_insert_sql(table_name, aggregate):
@@ -39,9 +43,9 @@ def final_insert_sql(table_name, aggregate):
 
     if aggregate:
         union = 'st_union(geom)'
-        group = 'group by nm_cliente, id_geoprocessamento'
+        group = 'group by id_geoprocessamento'
     return '''
-        insert into sd_producao.{0} (id_geoprocessamento, nm_cliente, geom) 
-        select id_geoprocessamento id_geoprocessamento, nm_cliente nm_cliente,  {1} geom 
+        insert into sd_producao.{0} (id_geoprocessamento, file_name, geom) 
+        select id_geoprocessamento id_geoprocessamento, file_name file_name,  {1} geom 
         from {0} {2};
     '''.format(table_name, union, group)
--- libs/utils.py
+++ libs/utils.py
@@ -1,4 +1,4 @@
-import unidecode, re, json, os, shutil, zipfile
+import unidecode, re, json, os
 from osgeo import ogr
 from .sql_generators import insert_geom_sql
 
@@ -14,7 +14,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[-1].lower() in ALLOWED_EXTENSIONS
 
 
-def fix_multigeometric(feat_json, filename_slug, buffer):
+def fix_multigeometric(feat_json, buffer, filename):
     polygons_coordinates = []
     linestrings_coordinates = []
     geometries = feat_json['geometry']['geometries']
@@ -30,18 +30,19 @@ def fix_multigeometric(feat_json, filename_slug, buffer):
     if len(polygons_coordinates):
         feat_json['geometry']['type'] = 'MultiPolygon'
         feat_json['geometry']['coordinates'] = polygons_coordinates
-        return insert_geom_sql(feat_json, filename_slug, buffer)
+        return insert_geom_sql(feat_json, buffer, filename)
     if len(linestrings_coordinates):
         feat_json['geometry']['type'] = 'MultiLineString'
         feat_json['geometry']['coordinates'] = linestrings_coordinates
-        return insert_geom_sql(feat_json, filename_slug, buffer)
+        return insert_geom_sql(feat_json, buffer, filename
+                               )
 
 
 def create_inserts(temp_folder, table_name, buffer) -> list:
     sql_inserts = []
     for filename in os.listdir(temp_folder):
-        filename_slug = slugify(filename.lower().split(".")[0].replace(" ", ""))
-        print("--> " + filename_slug)
+        # filename_slug = slugify(filename.lower().split(".")[0].replace(" ", ""))
+        print("--> " + filename)
         # Read XML file into python
         driver = ogr.GetDriverByName('KML')
         datasource = driver.Open(temp_folder + '/' + filename)
@@ -56,16 +57,15 @@ def create_inserts(temp_folder, table_name, buffer) -> list:
                 feat_json = json.loads(feat.ExportToJson())
 
                 if 'geometries' in feat_json['geometry']:
-                    insert_statement = fix_multigeometric(feat_json, filename_slug, buffer)
+                    insert_statement = fix_multigeometric(feat_json, buffer, filename)
                 else:
-                    insert_statement = insert_geom_sql(feat_json, filename_slug, buffer)
+                    insert_statement = insert_geom_sql(feat_json, buffer, filename)
 
                 sql_inserts.append(insert_statement)
 
                 feat = layer.GetNextFeature()
 
     return '''
-        insert into {0} (id_geoprocessamento, nm_cliente, geom) 
+        insert into {0} (id_geoprocessamento, file_name, geom) 
         values {1} ;
-    '''.format(table_name,','.join(sql_inserts))
-
+    '''.format(table_name, ','.join(sql_inserts))
